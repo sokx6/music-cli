c80b1436cd1335154c0b7b918059086d69f38b2e
locxl <sokx126@gmail.com>
2025-10-25 14:18:43 +0800

    refactor: 清理歌词模块，移除调试输出并整理解析与打印流程，采用定时器打印

--- player/lyric.go
+++ player/lyric.go
@@ -2,7 +2,9 @@ package player
 
 import (
 	"fmt"
+	"music-cli/utils"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -23,39 +25,10 @@ func NewLyric(lines []LyricLine) *Lyric {
 	}
 }
 
-func (l *Lyric) GetCurrentLyric(currentTime time.Duration) string {
-	// 如果歌词为空，直接返回
-	if len(l.LyricLines) == 0 {
-		l.currentIndex = -1
-		return ""
-	}
-
-	// 如果索引还没初始化，或者当前时间小于了当前索引指向的时间（比如用户拖动进度条到开头），则重置索引
-	if l.currentIndex == -1 || currentTime < l.LyricLines[l.currentIndex].Time {
-		l.currentIndex = 0
-	}
-
-	// 从当前索引开始，向后查找，直到找到第一个时间大于当前时间的歌词行
-	// 那么我们要找的歌词就是这一行的前一行
-	for l.currentIndex < len(l.LyricLines) && currentTime >= l.LyricLines[l.currentIndex].Time {
-		l.currentIndex++
-	}
-
-	// 如果索引超出了范围，说明歌曲已经播完所有歌词
-	if l.currentIndex == 0 {
-		return "" // 还没到第一句歌词
-	}
-
-	// 返回前一句歌词，它就是当前时间点应该播放的歌词
-	return l.LyricLines[l.currentIndex-1].Text
-}
-
 func (l *Lyric) ParseLyric(rawLyrics string) {
-	fmt.Printf("Parsing raw lyrics:%s\n", rawLyrics)
 
 	for _, line := range strings.Split(rawLyrics, "\n") {
 		duration := time.Duration(0)
-		fmt.Println("Parsing line:", line)
 		if len(line) < 10 || line[0] != '[' || line[9] != ']' {
 			continue
 		}
@@ -67,11 +40,10 @@ func (l *Lyric) ParseLyric(rawLyrics string) {
 		var minute, second, millisecond int
 		_, err := fmt.Sscanf(timeStr, "%02d:%02d.%03d", &minute, &second, &millisecond)
 		if err != nil {
-			fmt.Println("Failed to parse duration:", err)
+			/* fmt.Println("Failed to parse duration:", err) */
 			continue
 		}
 		duration = time.Duration(minute)*time.Minute + time.Duration(second)*time.Second + time.Duration(millisecond)*time.Millisecond
-		fmt.Printf("Parsed line - Time: %v, Text: %s\n", duration, text)
 		// 添加到歌词对象中
 		l.LyricLines = append(l.LyricLines, LyricLine{
 			Time: duration,
@@ -82,4 +54,38 @@ func (l *Lyric) ParseLyric(rawLyrics string) {
 
 func (l *Lyric) GetAllLyrics() []LyricLine {
 	return l.LyricLines
+}
+
+func (l *Lyric) printLyric(wg *sync.WaitGroup) {
+	defer wg.Done()
+	start := time.Now()
+	var lastLyric string
+	var currentLyric string
+	var nextLyric string
+	for i, ly := range l.LyricLines {
+		currentLyric = ly.Text
+		if i < len(l.LyricLines)-1 {
+			nextLyric = l.LyricLines[i+1].Text
+		}
+		if i > 0 {
+			lastLyric = l.LyricLines[i-1].Text
+		}
+		target := start.Add(ly.Time)
+		now := time.Now()
+
+		timer := time.NewTimer(target.Sub(now))
+		<-timer.C
+
+		printMu.Lock()
+		fmt.Print("\033[4;1f")
+		fmt.Print("\033[2K")
+		fmt.Print(utils.Center(lastLyric))
+		fmt.Print("\033[6;1f")
+		fmt.Print("\033[2K")
+		fmt.Print(utils.Center("➣ " + currentLyric))
+		fmt.Print("\033[8;1f")
+		fmt.Print("\033[2K")
+		fmt.Print(utils.Center(nextLyric))
+		printMu.Unlock()
+	}
 }
