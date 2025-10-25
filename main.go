6ddbf5919934d96fe72ecf699331d7e334a24d3b
locxl <sokx126@gmail.com>
2025-10-25 14:20:11 +0800

    feat: 支持自定义音乐文件路径

--- main.go
+++ main.go
@@ -7,14 +7,15 @@ import (
 )
 
 func main() {
-	player := player.NewPlayer("test.mp3")
+	fmt.Println("请输出音乐路路径：")
+	var path string
+	fmt.Scanln(&path)
+	player := player.NewPlayer(path)
 	if err := player.Init(); err != nil {
 		log.Fatal(err)
 	}
 	defer player.Close()
-	fmt.Println("Playing audio...")
 	if err := player.Play(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Playback finished.")
 }
